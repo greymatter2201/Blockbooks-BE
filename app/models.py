f47a7d15a29edab0050a79d2fd5c6e602493dcce
kzy <kzy@yeetmail.com>
2022-09-08 13:16:51 +0700

    Added new database models

--- app/models.py
+++ app/models.py
@@ -3,34 +3,84 @@ from sqlalchemy.dialects.postgresql import JSON
 
 # Note: nullable is False by default when primary_key=True
 # Wrapper for setting column nullable to False
-def NullColumn(*args,**kwargs):
+def NotNull(*args,**kwargs):
     kwargs["nullable"] = kwargs.get("nullable",False)
     return db.Column(*args,**kwargs)
 
-# class Transactions(db.Model):
-#     __tablename__='transactions'
+class Transactions(db.Model):
+    tx_hash = db.Column(db.String(66), primary_key=True)
+    block_number = NotNull(db.Integer)
+    address = db.Column(db.String(42), db.ForeignKey('wallet.address'))
+    from_addr = NotNull(db.String(42))
+    to_addr = NotNull(db.String(42))
+    tx_timestamp = NotNull(db.Integer)
+    tx_value = NotNull(db.Integer)
+    tx_gas = NotNull(db.Integer)
+    tx_gas_price = NotNull(db.Integer)
+    tx_action = db.Column(db.String(20))
+    eth_rate = db.Column(db.Float)
 
-# class Wallet(db.Model):
-#     __tablename__='wallet'
+    def __repr__(self):
+        return f'<Tx {self.tx_hash}, Address {self.address}>'
+
+class transaction_details(db.Model):
+    tx_hash = db.Column(db.String(66), db.ForeignKey('transactions.tx_hash'), primary_key=True)
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+    memo = db.Column(db.String(120))
+    labels = db.Column(db.Integer, db.ForeignKey('labels.id'))
+
+class Wallet(db.Model):
+    address = db.Column(db.String(42), primary_key=True)
+    is_active = NotNull(db.Boolean)
+    latest_block = NotNull(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    transactions = db.relationship('Transactions', backref='wallet_address', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<Wallet {self.address}>'
 
 class User(db.Model):
-    __tablename__='users'
-
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(120))
+    wallets = db.relationship('Wallet', backref='wallet_user', lazy='dynamic')
+    contacts = db.relationship('Contact', backref='created_by', lazy='dynamic')
+    labels = db.relationship('label_schema', backref='label_user', lazy='dynamic')
 
     def __repr__(self):
         return f'<User {self.email}, ID {self.id}>'
 
-# class Contact(db.Model):
-#     __tablename__='contact'
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = NotNull(db.String(64), index=True)
+    address = NotNull(db.String(42), index=True)
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-# class label_schema(db.Model):
-#     __tablename__='label_schema'
+    def __repr__(self):
+        return f'<Contact {self.name}, Address {self.address}>'
 
-# class transaction_details(db.Model):
-#     __tablename__='transaction_details'
+class label_schema(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label_type = NotNull(db.String(20))
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+    address = db.Column(db.String(42), db.ForeignKey('wallet.address'))
+    from_addr = db.Column(db.String(42))
+    to_addr = db.Column(db.String(42))
+    amount = NotNull(db.Integer)
+    memo = db.Column(db.String(120))
+    labels = db.Column(db.Integer, db.ForeignKey('labels.id'))
 
-# class Labels(db.Model):
-#     __tablename__='labels'
+    def __repr__(self):
+        return f'<Label Schema {self.id}>'
+
+class Labels(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+    label = db.Column(db.String(64))
+    is_active = NotNull(db.Boolean)
+    label_schemas = db.relationship('label_schema', backref='label', lazy='dynamic')
+    transaction_details = db.relationship('transaction_details', backref='tx_detail_label', lazy='dynamic')
+    
+
+    def __repr__(self):
+        return f'<Label {self.id} {self.name}'
